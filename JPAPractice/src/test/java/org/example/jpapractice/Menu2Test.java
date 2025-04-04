1e7b75f519064d12a9911acc63f9adcb330364b0
Yongwook <kyw9885@gmail.com>
2025-04-04 09:57:10 +0900

    [test]

--- JPAPractice/src/test/java/org/example/jpapractice/Menu2Test.java
+++ JPAPractice/src/test/java/org/example/jpapractice/Menu2Test.java
@@ -1,11 +1,9 @@
 package org.example.jpapractice;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
 
 @SpringBootTest
 public class Menu2Test {
